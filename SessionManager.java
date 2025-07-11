242c665c741b9af0ad7b47cdd1cb676eb41ee5cb
AngeloCassano <angel96ka@gmail.com>
2025-07-11 16:33:00 +0200

    Update SessionManager.java

--- SessionManager.java
+++ SessionManager.java
@@ -1,12 +1,47 @@
-public class SessionManager {
-    static private SessionManager currentSession;
+import java.util.ArrayList;
 
-    private SessionManager(){};
+public class SessionManager {//classe SessionManager hce mantiene lo stato della sessione utente connesso
+    static private SessionManager currentSession; //attributo statico dell'unica istanza del SessionManager
+    Utente currentUser = new Utente(); //utente attualmente connesso alla sessione
+    ArrayList<Utente> listTeamUsers = new ArrayList<>();//lista degli utenti nel team
 
-    public static SessionManager instanceSessionManager(){
+    private SessionManager(){}; //costruttore privato non richiamabile
+
+    public static SessionManager getInstanceSessionManager(){ //metodo che restituisce il riferimento all'unica istanza di SessionManager
         if(currentSession==null){
             currentSession = new SessionManager();
         }
         return currentSession;
     }
+
+}
+
+class Utente{//classe che modella gli utenti all'interno del gioco
+    private String nomeUtente; //nome dell'utente
+    
+    private String password; //password dell'utente
+
+    public String getNomeUtente() { //metodo che restituisce il nome dell'utente
+        return nomeUtente;
+    }
+    
+    public void setNomeUtente(String nomeUtente) { //metodo che cambia il nome dell'utente
+        this.nomeUtente = nomeUtente;
+    }
+
+    private String getPassword() { //metodo privato che restituisce la password dell'utente
+        return password;
+    }
+
+    public boolean checkPassword(String password) {//metodo che verifica la password dell'utente, true se corretta, false in caso contrario
+        if(this.getPassword().equals(password)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+
 }
